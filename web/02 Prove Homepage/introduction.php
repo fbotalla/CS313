442768663801858ed98ebbd844f9423788b7465a
fbotalla <botalla.fabrizio@gmail.com>
2020-01-08 10:13:37 -1000

    Continuation

--- web/02 Prove Homepage/introduction.php
+++ web/02 Prove Homepage/introduction.php
@@ -2,17 +2,35 @@
 <html lang="en">
     <head>
 
+        <link rel="stylesheet" href="introduction.css">
 
     </head>
 
     <body>
 
-    <p>Testing php</p>
+    <p style="text-align: left;"><a href="index.html">Return to the index here</a></p>
+    <h1 style="text-align: center;">Fabrizio Botalla</h1>
+     
+    <div class="main-grid">
+            <div class="grid">
+                <h2>Introduction<?php echo " Today is " . date("Y/m/d");?></h2>
 
-        <?php 
-        echo " Today is " . date("Y/m/d");
-        ?>
+                <p id = "text">My name is Fabrizio Botalla, I am originally from Torino Italy. My wife and I are currectly living in Oahu, Hawaii. One of my favorite things to do is to play soccer.
+                One thing about me is that if I am passionate about something I always strive to be the best at it. While I was definitely not the best at playing soccer, I have gotten to certain levels that I never thought I would.
+                Recently I started to develop a strong passion for programming, I hope to exceed my expecations there too. 
+                My biggest accomplishment in life was to marry my college sweetheart. We have been happily married for almost two years. We love to spend time together and do fun activities.
+                While living in Europe, we travelled to 10 countries in one year! I consider myself a travel lover.<br>
+                One of my strongest qualities is the fact that I am a good problem solver. In many of my previous jobs I was recognized for this quality of mine. If I cannot solve a problem 
+                I usually take the necesary time to reasearch and come to a solution. This quality of mine applies to both programming and also daily lives activities. 
+                For the past two years I have been working in the IT department for two companies. While I enjoy working as an IT, I am excited to start my career in software development.
+                If you have any good references for jobs around your area that I could look into please contact me here: botalla.fabrizio@gmail.com.</p>
+            </div>
 
+            <div class="grid">
+                <p id = "picture">Picture of my wife and I during our travels</p>
+                <img src="IMG_9011.jpg" alt="Android" height="250px"; width="350">
+            </div>
+    </div>
     </body>
 
 </html>
