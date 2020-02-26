8b3afb5abd4ad95721faf4a8b0bd12f48ea12971
fbotalla <botalla.fabrizio@gmail.com>
2020-02-25 21:24:34 -1000

    final helloWord node

--- web/Node/HelloWorld.js
+++ web/Node/HelloWorld.js
@@ -2,9 +2,11 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+var json_Obj;
+var json;
 
 var server = http.createServer(onRequest);
-server.listen(8888);
+server.listen(8000);
 
 
 
@@ -14,13 +16,26 @@ function onRequest(request, response){
 
     var path = q.pathname;
 
-    if(path == '/hime'){
+    if(path == '/home'){
+        response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write("<h1>Hello World!!!!</h1>");
+    
     }else if(path == '/getData'){
-
-        response.write('{"name":"Br. Burton","class":"cs313"}');
-    }else{
-        response.write("404 Page not FOUND!");
+        json_Obj = fs.readFileSync("json.json");
+        json = JSON.parse(json_Obj);
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.write("My Name: " + json.name + "\n");
+        response.write("My Class: " + json.class + "\n");
+    }else if(path == '/beyond'){
+        response.writeHead(301,
+            {Location: 'http://google.com'}
+          );
+    }
+    
+    else{
+        response.writeHead(404, { "Content-Type": "text/html" });
+        response.write('404: Page Not Found');
+       
     }
    
     response.end();
